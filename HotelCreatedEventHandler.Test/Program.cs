7fefa515d86a774df5341b5562bf27e72a80b402
Ade <ade.akingbade@justeattakeaway.com>
2025-02-04 12:47:31 +0000

    Initial commit

---
+++ HotelCreatedEventHandler.Test/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using Amazon.Lambda.SNSEvents;
+using HotelCreatedEventHandler.Models;
+
+Environment.SetEnvironmentVariable("host", "https://search-hotels-25xqscxxtzr5irwawuexmodvbq.aos.eu-west-2.on.aws/_dashboards");
+Environment.SetEnvironmentVariable("userName", "elastic");
+Environment.SetEnvironmentVariable("password", "");
+Environment.SetEnvironmentVariable("indexName", "event");
+
+var hotel = new Hotel()
+{
+    Name = "Hotel",
+    CityName = "Seattle",
+    Price = 1,
+    Rating = 5,
+    Id = "123",
+    UserId = "ABC",
+    CreationDateTime = DateTime.Now
+};
+
+var snsEvent = new SNSEvent()
+{
+    Records = new List<SNSEvent.SNSRecord>()
+    {
+        new SNSEvent.SNSRecord()
+        {
+            Sns = new SNSEvent.SNSMessage()
+            {
+                MessageId = "100",
+                Message = JsonSerializer.Serialize(hotel)
+            }
+        }
+    }
+};
+
+var handler = new HotelCreatedEventHandler.HotelCreatedEventHandler();
+await handler.Handler(snsEvent);
---
+++ HotelCreatedEventHandler/HotelCreatedEventHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.Lambda.SNSEvents;
+using HotelCreatedEventHandler.Models;
+using Nest;
+
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
+
+namespace HotelCreatedEventHandler;
+
+public class HotelCreatedEventHandler
+{
+    public async Task Handler(SNSEvent snsEvent)
+    {
+        // note: document model instead of object persistence model (latter is the "orm" way)
+        var dbClient = new AmazonDynamoDBClient();
+        var table = Table.LoadTable(dbClient, "HotelCreatedEventIds");
+        
+        var host = Environment.GetEnvironmentVariable("host");
+        var userName = Environment.GetEnvironmentVariable("userName");
+        var password = Environment.GetEnvironmentVariable("password");
+        // elasticsearch namespace
+        var indexName = Environment.GetEnvironmentVariable("indexName");
+                
+        var connSettings = new ConnectionSettings(new Uri(host));
+        connSettings.BasicAuthentication(userName, password);
+        connSettings.DefaultIndex(indexName);
+        // ensures hotel.id is used as the id of the document in elasticsearch (instead of it gen'ing a random one)
+        connSettings.DefaultMappingFor<Hotel>(m => m.IdProperty(p => p.Id));
+                
+        var esClient = new Nest.ElasticClient(connSettings);
+        
+        // AWS may send more than 1 event at a time
+        foreach (var eventRecord in snsEvent.Records)
+        {
+            var eventId = eventRecord.Sns.MessageId;
+            var foundItem = await table.GetItemAsync(eventId);
+            if (foundItem == null)
+            {
+                await table.PutItemAsync(new Document()
+                {
+                    ["eventId"] = eventId
+                });
+                
+                // Ensuring the index exists in elasticsearch
+                if (!(await esClient.Indices.ExistsAsync(indexName)).Exists)
+                {
+                    await esClient.Indices.CreateAsync(indexName);
+                }
+
+                var hotel = JsonSerializer.Deserialize<Hotel>(eventRecord.Sns.Message);
+                await esClient.IndexDocumentAsync<Hotel>(hotel);
+            }
+        }
+    }
+}
